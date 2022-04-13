766f0cdd5661b44935e15c0cbad796038e014248
Yuting-Zhao-716 <zhaoyuting0716@gmail.com>
2022-04-13 20:39:41 +1200

    changed session-mongodb-url

--- config/session.js
+++ config/session.js
@@ -2,9 +2,13 @@ const session = require('express-session');
 const mongodbStore = require('connect-mongodb-session');
 
 function getSessionStore(){
+    let mongodbUrl='mongodb://localhost:27017';
+    if(process.env.MONGODB_URL){
+        mongodbUrl=process.env.MONGODB_URL;
+    }
     const MongodbStore = mongodbStore(session);
     const sessionStore=new MongodbStore({
-        uri:'mongodb://localhost:27017',
+        uri:mongodbUrl,
         databaseName: 'SpeedfactoryNZ',
         collection:'sessions'});
     return sessionStore;
